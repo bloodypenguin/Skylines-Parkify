cd11f4ab5099e8cf79f9fccf805a5c66e4f8602f
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2021-05-11 21:29:46 +0200

    Bump version to 1.0.5

--- Parkify/ParkifyMod.cs
+++ Parkify/ParkifyMod.cs
@@ -5,7 +5,7 @@ namespace Parkify
 {
     public class ParkifyMod : IUserMod
     {
-        public string Name => "Parkify r1.0.4";
+        public string Name => "Parkify r1.0.5";
         public string Description => "Allows to use the whole wealth of your park buildings as park modules";
 
         public void OnSettingsUI(UIHelperBase helper)
